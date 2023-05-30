f3d21b002a786256b82dfd8ecb146d8460580f2a
Ernyson <ernestaiji1@gmail.com>
2023-05-30 13:33:12 +0000

    worker

--- worker.js
+++ worker.js
@@ -62,14 +62,15 @@ userQueue.process(async (job, done) => {
   }
   console.log(`Welcome ${user.email}!`);
   try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
+    const mailSubject = 'Welcome to ALX-Files_Manager by Ernyson and Stellacandy';
     const mailContent = [
       '<div>',
       '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
+      'Welcome to <a href="https://github.com/Stellacandy/alx-files_manager">',
       'ALX-Files_Manager</a>, ',
       'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
+      '<a href="https://github.com/Stellacandy">Stellacandy</a>. ',
+	'<a href="https://github.com/Stellacandy">Stellacandy</a>. ',
       'We hope it meets your needs.',
       '</div>',
     ].join('');
